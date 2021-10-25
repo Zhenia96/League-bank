9dcd92eacf5bef472df4c00622355d2cbbcc749c
Yauheni <kirichenkozhenia@mail.ru>
2021-10-25 19:23:17 +0300

    Add normalize.css to the project

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import './css/fonts.scss';
+import 'normalize.css';
+import './css/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
